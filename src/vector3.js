b4573eeb5d3aa99423262003852ba412347ccde8
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-09-26 20:14:04 +0200

    rename vector create function to fromXYZ

--- src/vector3.js
+++ src/vector3.js
@@ -1,18 +1,29 @@
-export const create = (x, y, z) => ({
+export const fromXYZ = (x, y, z) => ({
     x,
     y,
     z
 })
 
-export const add = (a, b) => create(a.x + b.x, a.y + b.y, a.z + b.z)
+export const add = (a, b) => fromXYZ(a.x + b.x, a.y + b.y, a.z + b.z)
+
 export const cross = (a, b) => ({
     x: a.x * b.z - a.z * b.y,
     y: a.z * b.x - a.x * b.z,
     z: a.x * b.y - a.y * b.x
 })
+
 export const dot = (a, b) => a.x * b.x + a.y * b.y + a.z * b.z
+
 export const length2 = v => dot(v, v)
+
 export const length = v => Math.sqrt(length2(v))
+
+export const negate = ({ x, y, z }) => fromXYZ(-x, -y, -z)
+
 export const normalize = v => scale(v, 1 / length(v))
-export const scale = ({ x, y, z }, f) => create(x * f, y * f, z * f)
-export const sub = (a, b) => create(a.x - b.x, a.y - b.y, a.z - b.z)
+
+export const scale = ({ x, y, z }, f) => fromXYZ(x * f, y * f, z * f)
+
+export const sub = (a, b) => fromXYZ(a.x - b.x, a.y - b.y, a.z - b.z)
+
+export const ZERO = fromXYZ(0, 0, 0)
