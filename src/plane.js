2ce048dd2015ffa62b2f51dc45919b9acc047d4b
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-09-30 13:05:36 +0200

    add support for plane geometry

---
+++ src/plane.js
@@ -0,0 +1,21 @@
+import { add, dot, scale } from './vector3.js'
+
+export const intersectPlaneRay = plane => ({
+    origin,
+    direction,
+    tMin = 0,
+    tMax = Infinity
+}) => {
+    const { d, basis } = plane
+    const { normal } = basis
+    const t = (d - dot(normal, origin)) / dot(normal, direction)
+    if (t > tMin && t < tMax) {
+        const point = add(origin, scale(direction, t))
+        return {
+            t,
+            point,
+            basis,
+            intersectable: plane
+        }
+    }
+}
