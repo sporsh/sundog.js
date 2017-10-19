26ff265e51868ece3798ae7c377fc9723aabedde
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-10-19 22:24:48 +0200

    add transmissive material

--- src/material.js
+++ src/material.js
@@ -1,4 +1,4 @@
-import { fromXYZ, ZERO, normalize, scale, sub, dot } from './vector3.js'
+import { ZERO, normalize, negate, scale, sub, dot } from './vector3.js'
 import { toStandardBasis } from './basis.js'
 
 const lambertianScatter = (outgoing, surface) => {
@@ -36,5 +36,38 @@ export const specularMaterial = ({ albedo, emittance = ZERO }) => ({
     pdf: () => albedo,
     scatter: (outgoing, { normal }) =>
         sub(scale(normal, 2 * Math.max(0, dot(normal, outgoing))), outgoing),
+    emittance: () => emittance
+})
+
+export const transmissiveMaterial = ({
+    albedo,
+    emittance = ZERO,
+    refractiveIndex
+}) => ({
+    pdf: () => albedo,
+    scatter: (outgoing, { normal }) => {
+        outgoing = negate(outgoing)
+        let eta
+        let cosi = dot(normal, outgoing)
+        if (cosi < 0) {
+            // Entering
+            eta = 1 / refractiveIndex
+        } else {
+            // Exiting
+            eta = refractiveIndex / 1
+            normal = negate(normal)
+        }
+        const cost2 = 1 - eta * eta * (1 - cosi * cosi)
+        if (cost2 < 0) {
+            // Total internal reflection
+            return sub(outgoing, scale(normal, 2.0 * cosi))
+        }
+        return normalize(
+            sub(
+                scale(outgoing, eta),
+                scale(normal, eta * Math.abs(cosi) + Math.sqrt(cost2))
+            )
+        )
+    },
     emittance: () => emittance
 })
