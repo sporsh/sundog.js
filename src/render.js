3f1de80daedf08eaed36c54b38a63c9b62da47ef
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-12-01 22:46:32 +0100

    move render and trace functions into separate modules

---
+++ src/render.js
@@ -0,0 +1,42 @@
+import { fromXYZ, add, scale } from './vector3.js'
+import { fromV3, toAbgr32 } from './color.js'
+
+export const renderRegion = (trace, cameraRayThrough) => (
+    spp,
+    x0,
+    y0,
+    x1,
+    y1,
+    width,
+    height
+) => {
+    const dx = x1 - x0
+    const dy = y1 - y0
+    const imageData = new ImageData(dx, dy)
+    const buffer32 = new Uint32Array(imageData.data.buffer)
+    const halfWidth = width / 2
+    const halfHeight = height / 2
+
+    for (let y = y0; y < y1; y++) {
+        for (let x = x0; x < x1; x++) {
+            const u = x / halfWidth - 1
+            const v = y / halfHeight - 1
+            let radiance = fromXYZ(0, 0, 0)
+            for (let n = spp; n > 0; n--) {
+                const ray = cameraRayThrough(
+                    u + (2 * Math.random() - 1) / dx,
+                    v + (2 * Math.random() - 1) / dy
+                )
+                radiance = add(
+                    radiance,
+                    trace(ray, fromXYZ(0, 0, 0), fromXYZ(1, 1, 1), 0)
+                )
+            }
+            const color = fromV3(scale(radiance, 1 / spp))
+            const i = x - x0 + (y - y0) * dx
+            buffer32[i] = toAbgr32(color)
+        }
+    }
+
+    return imageData
+}
---
+++ src/trace.js
@@ -0,0 +1,44 @@
+import { add, hadamard, scale } from './vector3.js'
+
+export const tracer = (intersect, maxDepth = 6, epsilon = 0.0001) => {
+    const trace = (ray, radiance, weight, depth) => {
+        const intersection = intersect(ray)
+        if (intersection) {
+            const {
+                t,
+                point,
+                normal,
+                basis,
+                intersectable: { material: { pdf, emittance, scatter } }
+            } = intersection
+            const prob = pdf(ray.direction, basis)
+            const pMax = Math.max(Math.max(prob.x, prob.y), prob.z)
+
+            const newRadiance = add(
+                radiance,
+                hadamard(emittance(ray.direction), weight)
+            )
+
+            //  Russian roulette (after a couple of bounces)
+            if (
+                depth > maxDepth ||
+                pMax <= 0 ||
+                (depth > 2 && Math.random() > pMax)
+            ) {
+                return newRadiance
+            }
+
+            const newRay = {
+                origin: point,
+                direction: scatter(ray.direction, basis),
+                tMin: epsilon,
+                tMax: Infinity
+            }
+            const newWeight = hadamard(weight, scale(prob, 1 / pMax))
+            return trace(newRay, newRadiance, newWeight, depth + 1)
+        } else {
+            return radiance
+        }
+    }
+    return trace
+}
