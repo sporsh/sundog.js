ece283013476efb5275567474f97370f4ca2e50d
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-12-03 17:06:10 +0100

    support triangle geometry

---
+++ src/triangle.js
@@ -0,0 +1,47 @@
+import { sub, cross, dot, add, negate, scale } from './vector3.js'
+import { arbitraryBasisForNormal } from './basis.js'
+
+export const intersectTriangleRay = triangle => {
+    const { a, b, c } = triangle
+
+    const ab = sub(b, a)
+    const ac = sub(c, a)
+    const normal = cross(ab, ac)
+
+    return ({ origin, direction, tMin = 0, tMax = Infinity }) => {
+        const qp = negate(direction)
+
+        const d = dot(qp, normal)
+        if (d === 0) {
+            // Ray is parallel to triangle
+            return
+        }
+        if (d < 0) {
+            // Ray is potentially hitting the triange from the back
+            return
+        }
+
+        const ap = sub(origin, a)
+        const t = dot(ap, normal) / d
+        if (t < tMin || t > tMax) {
+            return
+        }
+
+        const e = cross(qp, ap)
+        const v = dot(ac, e)
+        if (v < 0 || v > d) {
+            return
+        }
+        const w = -dot(ab, e)
+        if (w < 0 || v + w > d) {
+            return
+        }
+
+        return {
+            t,
+            point: add(origin, scale(direction, t)),
+            basis: arbitraryBasisForNormal(normal),
+            intersectable: triangle
+        }
+    }
+}
