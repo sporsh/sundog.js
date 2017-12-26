2c185d6b7fec5d40974852c2a9fa914d088d5782
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-12-26 14:16:08 +0100

    clean up aabb intersection test

--- src/box.js
+++ src/box.js
@@ -4,22 +4,34 @@ export const testBoxRay = ({ min, max }) => ({
     tMin = 0,
     tMax = Infinity
 }) => {
-    const d = [direction.x, direction.y, direction.z]
-    const p = [origin.x, origin.y, origin.z]
-    const mn = [min.x, min.y, min.z]
-    const mx = [max.x, max.y, max.z]
-
-    for (let i = 0; i < 3; ++i) {
-        const ood = 1 / d[i]
-        const t1 = (mn[i] - p[i]) * ood
-        const t2 = (mx[i] - p[i]) * ood
-
+    // X-slab
+    {
+        const ood = 1 / direction.x
+        const t1 = (min.x - origin.x) * ood
+        const t2 = (max.x - origin.x) * ood
+        tMin = Math.max(tMin, Math.min(t1, t2))
+        tMax = Math.min(tMax, Math.max(t1, t2))
+        if (tMin > tMax) return false
+    }
+    // Y-slab
+    {
+        const ood = 1 / direction.y
+        const t1 = (min.y - origin.y) * ood
+        const t2 = (max.y - origin.y) * ood
+        tMin = Math.max(tMin, Math.min(t1, t2))
+        tMax = Math.min(tMax, Math.max(t1, t2))
+        if (tMin > tMax) return false
+    }
+    // Z-slab
+    {
+        const ood = 1 / direction.z
+        const t1 = (min.z - origin.z) * ood
+        const t2 = (max.z - origin.z) * ood
         tMin = Math.max(tMin, Math.min(t1, t2))
         tMax = Math.min(tMax, Math.max(t1, t2))
         if (tMin > tMax) return false
     }
 
-    // return tmax > tmin
     return {
         origin,
         direction,
@@ -32,7 +44,5 @@ export const boundIntersectRay = (testRay, intersectRay) => ray => {
     const boundRay = testRay(ray)
     if (boundRay) {
         return intersectRay(boundRay)
-    }
-    if (testRay(ray) !== false) {
     }
 }
