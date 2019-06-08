f6de3d95b2ca46dd0ab6ff01de7421abaa04e92c
Geir Sporsheim <geir.sporsheim@gmail.com>
2019-06-08 14:08:22 +0200

    set up webpack and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
+/* eslint-env node, commonjs */
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkerPlugin = require('worker-plugin')
+
 module.exports = {
-    entry: { main: './main.js', worker: './worker.js' },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/dist'
-    }
+  mode: 'production',
+  entry: { main: './main.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html' }),
+    new WorkerPlugin()
+  ],
+  // devtool: "source-map"
+  optimization: {
+    minimize: false
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  }
 }
